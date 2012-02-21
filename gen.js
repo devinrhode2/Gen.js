e625c28cf2dc77d41ba46691b7067c2c9599ad7b
Devin Rhode <devinrhode2@gmail.com>
2012-02-21 14:50:59 -0800

    gen.js contains a String.has method, and an Array.each method

--- gen.js
+++ gen.js
@@ -1,134 +1,37 @@
-/* Gen.js: some stupid simple javascript functions
- * Assumes you aren't using jQuery
- * By Devin Rhode @DevinRhode2 */
-
-/* GET Example Call:
- * GET('http://thescouapp.com/X/genjs.json', function(response){
- *   console.error(response);
- * });
- *
- * Notes:
- * May run into issues with ajax call being cached.
- * A third options param would be nice to handle this and other things.
- * Alternatively, storing settings for GET within itself, like GET.cache = false would be great too */
-function GET(url, callback) {
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.open("GET",url + '?nocache='+Date.now() ,true);
-  xmlhttp.onreadystatechange = function() {
-    if (xmlhttp.readyState === 4) {
-      if (xmlhttp.status === 200) {
-        var resp = xmlhttp.responseText;
-        if (typeof callback !== 'undefined') {
-          try {
-            if (resp.indexOf('{') === 0) {
-              callback(JSON.parse(xmlhttp.responseText));
-            } else {
-              callback(xmlhttp.responseText);
-            }
-          } catch(e) { console.error(e); }
-        } else {
-          console.error(xmlhttp.responseText);
-        }
-      } else {
-        console.error('GET error for: ' + url);
-        console.error('xhr:');
-        console.error(xmlhttp);
-      }
-    }
-  }
-  xmlhttp.send();
-}
-
-/* Simpler version
-function GET(url, callback) {
-  var xmlhttp = new XMLHttpRequest();
-  
-  //prevent caching
-  if (url.indexOf('?') > -1) {
-    xmlhttp.open("GET",url + '&nocache='+Date.now() ,true);
+/**
+ * has: Does a url have a certain substring inside it?
+ * Example:
+ * if (url.has('https://')) {
+ *   console.error('This is a normal secure page!');
+ * }
+ */
+String.prototype.has = function(string) {
+  if (this.indexOf(string) > -1) {
+      return true;
   } else {
-    xmlhttp.open("GET",url + '?nocache='+Date.now() ,true);
+      return false;
   }
-  
-  xmlhttp.onreadystatechange = function() {
-    if (xmlhttp.readyState === 4) {
-      if (xmlhttp.status === 200) {
-        var resp = xmlhttp.responseText;
-        if (typeof callback !== 'undefined') {
-          callback(xmlhttp.responseText);
-        } else {
-          console.error(xmlhttp.responseText);
-        }
-      } else {
-        console.error('GET error for: ' + url);
-        console.error('xhr:');
-        console.error(xmlhttp);
-      }
-    }
-  }
-  xmlhttp.send();
-}*/
+};
 
-
-/* Example: 
- * POST('example.php', 'key=val&note=yeah'); 
- * -- data may also work as a javascript object, haven't tried this */
-function POST(url, data, callback) {
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.open("POST",url + '?nocache='+Date.now() ,true);
-  xmlhttp.onreadystatechange = function() {
-    if (xmlhttp.readyState === 4) {
-      if (xmlhttp.status === 200) {
-        var resp = xmlhttp.responseText;
-        if (typeof callback !== 'undefined') {
-          try {
-            if (resp.indexOf('{') === 0) {
-              callback(JSON.parse(xmlhttp.responseText));
-            } else {
-              callback(xmlhttp.responseText);
-            }
-          } catch(e) { console.error('caught:'); console.error(e); }
-        } else {
-          console.error(xmlhttp.responseText);
-        }
-      } else {
-        console.error('POST error for: ' + url);
-        console.error('xhr:');
-        console.error(xmlhttp);
-      }
-    }
-  }
-  xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-  xmlhttp.send(data);
-}
-
-/* each: Call action() on each item in an array
+/**
+ * each as a method on Array - Call .action() on each item in an array
  * Example:
  * var arr = [1, 2, 3];
- * each(arr, console.error); */ 
-function each(darnArray, action) {
-  var numItems = darnArray.length;
-  for (var i = 0; i < numItems; i++) {
-    action(darnArray[i]);
+ * arr.each(console.error);
+ */ 
+Array.prototype.each = function(functionObject) {
+  //validation
+  if (this.length === 0) {
+    console.error('Array is empty, doing nothing.');
+    return 'Array is empty, doing nothing.';
   }
-}
-
-/* has: Does a url have a certain substring inside it?
- * Example:
- * has.url = location.href;
- * if (has('https://')) {
- *   console.error('This is a normal secure page!');
- * } */
-function has(string) {
-  if (typeof has.url === 'undefined') {
-    alert('you need to set has.url to a url.');
-  } else {
-    if (has.url.indexOf(string) > -1) {
-      return true;
-    } else {
-      return false;
-    }
+  if (typeof functionObject !== 'function') {
+    throw '.each was called with a non-function object to call on each array item. Failing.';
+    return false;
   }
-}
-
-
+  var numItems = this.length;
+  var start = 0;
+  for (; start < numItems ;start++) {
+    functionObject(this[start]);
+  }
+};
